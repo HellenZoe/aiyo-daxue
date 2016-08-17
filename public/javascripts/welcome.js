cd850498935508b5f63bea9c040b002efb40117b
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-18 03:38:02 +0800

    fix style problem

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -61,7 +61,7 @@ QC.api("get_user_info", paras)
 
 		//  让去完善的按钮带上相应用户的参数
 		var oldHref = $('.gotoSelf').attr('href');
-		var newHref = oldHref + "?avatarUrl=" + avatarUrl
+		var newHref = oldHref + "?userAavatarUrl=" + avatarUrl
 		$('.gotoSelf').attr('href', newHref);
 
 		// 隐藏加载
