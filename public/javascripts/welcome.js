3e53d2b8bb0b9721573185908d3cebe4ecd351e0
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-18 17:50:30 +0800

    debug

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -67,7 +67,7 @@ QC.api("get_user_info", paras)
 		//  将用户信息存储到localstorage 每次进入到首页用ajax获取数据
 
 		if (window.utils) {
-			utils.saveToLocal("userInfo", userInfo);
+			window.utils.saveToLocal("userInfo", userInfo);
 			console.log("save userInfo success", window.getFromLocal("userInfo"));
 		}
 
