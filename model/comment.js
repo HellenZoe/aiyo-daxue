16801bb02013529af141b4f91a1ab03c96529b63
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-20 00:10:06 +0800

    debug

--- model/comment.js
+++ model/comment.js
@@ -20,12 +20,32 @@ commentSchema.virtual('date').get(function() {
   var p = new Date();
   d.setTime(this.time);
   p.setTime(n);
+  var year = p.getYear() - d.getYear();
   var month = p.getMonth()  -d.getMonth();
+  var date = p.getDate() - d.getDate();
   var hour = p.getHours() - d.getHours();
   var minute = p.getMinutes() - d.getMinutes();
-  if (month > 1) {
+  if (year > 1) {
     return "超久了"
   }else {
+    if ( (month = 1 && date > 0) || (month >= 2)) {
+      return "一个月以前"
+    }else{
+      if (mon =1 && date < 0) {
+        return 30 + date + "天之前";
+      }else {
+        if (date >= 1) {
+          return date + "天之前";
+        }else{
+          if (hour >= 1) {
+            return hour + "小时之前";
+          }else {
+              return minute + "分钟之前";
+          }
+
+        }
+      }
+    }
     return minute + "分钟之前";
   }
 })
