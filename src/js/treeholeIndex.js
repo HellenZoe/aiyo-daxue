afa11441cb0da07b5a3fd9f69704df53ea2d500e
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-19 16:01:36 +0800

    debug

---
+++ src/js/treeholeIndex.js
@@ -0,0 +1,6 @@
+//   点击图片的时候  跳转到相应的详情页去
+$('.facebook-card').on('click', function(e) {
+  var cardId = $(this).attr('data-tId');
+  var host = location.host;
+  location.href = "http://" + host + "/treehole/detail" + cardId;
+})
