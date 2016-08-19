474e172b80f2ca8a752ef679f85baff0bb92bb1e
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-19 23:14:55 +0800

    debug

--- model/comment.js
+++ model/comment.js
@@ -13,6 +13,24 @@ var commentSchema = new Schema({
   type: String
 })
 
+
+treeholeSchema.virtual('date').get(function() {
+  var n = Date.now();
+  var d = new Date();
+  var p = new Date();
+  d.setTime(this.time);
+  p.setTime(n);
+  var month = p.getMonth()  -d.getMonth();
+  var hour = p.getHours() - d.getHours();
+  var minute = p.getMinutes() - d.getMinutes();
+  minute = (minute >= 10) ? minute : "0" +  minute;
+  if (month > 1) {
+    return "超久了"
+  }else {
+    return hour+ "个小时" + minute + "分钟之前";
+  }
+})
+
 var Comment = mongoose.model('Comment', commentSchema);
 
 module.exports =  Comment;
