3335c5b90f61163ea31526d0d9e493abfe2e0ef7
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-20 02:05:44 +0800

    debug

--- public/javascripts/treeholeIndex.js
+++ public/javascripts/treeholeIndex.js
@@ -4,3 +4,43 @@ $('.facebook-card').on('click', function(e) {
   var host = location.host;
   location.href = "http://" + host + "/treehole/detail/" + cardId;
 })
+
+$('.iconfont-nullEnjoy').on('click', function(e) {
+  var enjoyCount = $(this).parent().parent().children('#enjoy').children('enjoy-count').text();
+  var treeholeId = $(this).parent().parent().parent().attr(data-tId);
+  var countInfo = {
+    fav: enjoyCount,
+    treeholeId: treeholeId
+  }
+
+  var url = "http://" + location.host + "/treehole/fav";
+
+  //  显示加载指示器
+  $.showPreloader();
+
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    dataType: "json",
+		contentType: "application/json",
+    data: JSON.stringify(countInfo),
+    processData: false,
+    success: function (data) {
+      if (data.success) {
+        console.log("上传成功");
+
+        $.hidePreloader();
+        $.toast('点赞成功', 2000, "toast-success");
+        //  点赞数加1
+        $('.enjoy-count').text(data.c);
+      }
+    },
+    error: function (data) {
+        // showMessageFail("上传出错, 请重试");
+        console.log("上传失败");
+    }
+
+  });
+
+})
