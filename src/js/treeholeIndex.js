fe97cd32ab9663a9b49944094dca129e9c0341a0
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-20 14:33:08 +0800

    debug

--- src/js/treeholeIndex.js
+++ src/js/treeholeIndex.js
@@ -4,9 +4,10 @@ allCard.forEach(function(item, index) {
   var crtUserId = window.utils.getFromLocal('userInfo')._id;
   item = $(item);
   var favId  = item.attr('data-favId');
+  console.log(favId);
   if (favId.contains(crtUserId)) {
-    item.children('.topic-card-footer').children('#enjoy').children('iconfont-nullEnjoy').css('display', 'none')
-    item.children('.topic-card-footer').children('#enjoy').children('iconfont-selfEnjoy').css('display', 'inline')
+    item.children('.topic-card-footer').children('#enjoy').children('.iconfont-nullEnjoy').css('display', 'none')
+    item.children('.topic-card-footer').children('#enjoy').children('.iconfont-selfEnjoy').css('display', 'inline')
   }
 })
 
