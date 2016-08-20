2e3418e169f321aa4dcfc7c7db88a443af7f1ca7
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-20 14:23:37 +0800

    debug

--- public/javascripts/treeholeIndex.js
+++ public/javascripts/treeholeIndex.js
@@ -2,6 +2,7 @@
 var allCard = $('.facebook-card');
 allCard.forEach(function(item, index) {
   var crtUserId = window.utils.getFromLocal('userInfo')._id;
+  item = $(item);
   var favId  = item.attr('data-favId');
   if (item.attr(favId.contains(crtUserId))) {
     item.children('.topic-card-footer').children('#enjoy').children('iconfont-nullEnjoy').css('display', 'none')
