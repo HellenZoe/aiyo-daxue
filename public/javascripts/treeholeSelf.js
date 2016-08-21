d404df9ce4ed53f85f9cb03da9e948edc0efeaff
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-21 14:10:53 +0800

    debug

--- public/javascripts/treeholeSelf.js
+++ public/javascripts/treeholeSelf.js
@@ -1,3 +1,16 @@
 if (window.utils) {
   window.utils.saveToLocal('crt-service', "treehole");
 }
+
+
+var allCard = $('.topic-block');
+allCard.forEach(function(item, index) {
+  var crtUserId = window.utils.getFromLocal('userInfo')._id;
+  item = $(item);
+  var favId  = item.attr('data-favId');
+  console.log(favId);
+  if (favId.contains(crtUserId)) {
+    item.children('.topic-card-footer').children('#enjoy').children('.iconfont-nullEnjoy').css('display', 'none')
+    item.children('.topic-card-footer').children('#enjoy').children('.iconfont-selfEnjoy').css('display', 'inline')
+  }
+})
