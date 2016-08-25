4365bf393d9721750bb84397c916df8aaf595c19
liadbiz <zhuhaihao_hust@firefox.com>
2016-08-25 15:20:10 +0800

    debug

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
 $.init();
 
+if (!window.utils.getFromLocal('userInfo')) {
+  $.alert("没有登录", "登陆之后才能浏览", function() {
+    location.href = "http://" + location.host + "/signin";
+  });
+}
 $(function() {
   $(".swiper-container").swiper({
     autoplay: 2000,
-    spaceBetween: 15  
+    spaceBetween: 15
   })
 })
