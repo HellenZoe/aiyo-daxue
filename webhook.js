9e8e3307ca1f61d8769c826a20f12cacd6cb8136
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-02 10:46:33 +0800

    test webhook

--- webhook.js
+++ webhook.js
@@ -7,7 +7,7 @@ function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
   var child = spawn(cmd, args);
   var resp = "";
-
+  console.log("run cmd");
   child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
   child.stdout.on('end', function() { callback (resp) });
 }
