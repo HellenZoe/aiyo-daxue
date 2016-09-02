33b7477d6400b0c725f980ef0f18c8fa31ada762
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-02 11:14:24 +0800

    test webhook

--- bin/deploy-dev.sh
+++ bin/deploy-dev.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# WEB_PATH='/var/www/dev.lovelucy.info'
-# WEB_USER='lovelucydev'
-# WEB_USERGROUP='lovelucydev'
-#
-# echo "Start deployment"
-# cd $WEB_PATH
 LOG_FILE="/var/log/nodejs/aiyo/blog_deploy.log"
 date >> "$LOG_FILE"
 echo "Start deployment" >> "$LOG_FILE"
