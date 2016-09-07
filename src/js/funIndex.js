67240fda15561c73d35f9ac6a0d2a9d42b59cfb2
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-07 10:19:16 +0800

    add hint for no img in fun index page

--- src/js/funIndex.js
+++ src/js/funIndex.js
@@ -3,3 +3,11 @@ $('.item-block').on('click', function(e) {
   var host = location.host;
   location.href = "http://" + host + "/fun/detail/" + fId;
 })
+
+var imgs = $('.no-img');
+imgs.each(function(item, index) {
+  var i = $(item);
+  if (!i.attr(src)) {
+    i.parent().children('no-img').css('display', 'inline-block');
+  }
+})
