286fbdee339427e9319ff699427527514f565f8e
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-08 15:19:29 +0800

    fix bug

--- public/javascripts/funIndex.js
+++ public/javascripts/funIndex.js
@@ -1,9 +1,20 @@
+$.init();
+
+//  slider初始化
+$(function() {
+  $(".swiper-container").swiper({
+    autoplay: 2000,
+  })
+})
+
+// 点击详情页
 $('.item-block').on('click', function(e) {
   var fId = $(this).attr("data-fId");
-  var host = location.host;
+var host = location.host;
   location.href = "http://" + host + "/fun/detail/" + fId;
 })
 
+// 如果用户没有发布图片显示提示
 var imgs = $('.img-wrapper img');
 imgs.each(function(index, item) {
   var i = $(item);
