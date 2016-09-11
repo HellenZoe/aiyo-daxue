b552ac7aa50cc9c4ba480023b69985fd76b064e0
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-11 16:12:06 +0800

    debug

---
+++ src/js/imgViewer.js
@@ -0,0 +1,26 @@
+var imgWrappers = $('.picsWrapper');
+imgWrappers.forEach(function(item, index) {
+  console.log(item, index);
+
+  //  给每个图片容器都绑定一个点击大图预览事件
+  var wrapper = $(item);
+  wrapper.on('click', function(e) {
+    //  默认事件
+    e.preventDefault();
+
+    //  拿到图片链接
+    var urls = [];
+    var images = $(this).children('img');
+    images.forEach(function(item, index) {
+      console.log($(item));
+      images.push($(item).attr('href'))
+    })
+
+    var myPhotoBrowserStandalone = new $.photoBrowser({
+      photo: urls
+    })
+
+    // 打开图片浏览器
+    myPhotoBrowserStandalone.open();
+  })
+})
