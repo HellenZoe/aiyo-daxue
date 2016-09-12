8900383766e348cbba20772a1e108a01e1b274cb
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-12 09:31:28 +0800

    debug

--- public/javascripts/treeholePost.js
+++ public/javascripts/treeholePost.js
@@ -180,6 +180,7 @@ $(function() {
 
   function addCanvasToPreview(canvas, fileType) {
     var wrapper = document.createElement("div");
+    wrapper.className = "picWrapper";
     var deleteIcon = document.createElement("i");
     deleteIcon.className = "iconfont-postDelete";
     deleteIcon.innerHTML = "&#xe607;"
@@ -195,6 +196,47 @@ $(function() {
     allPic.pics.push(dataURL);
     console.log("new ", allPic.pics);
     picCount = picCount + 1;
+
+
+
+// 大图预览
+var imgWrappers = $('.picsWrapper');
+imgWrappers.forEach(function(item, index) {
+  console.log(item, index);
+
+  //  给每个图片容器都绑定一个点击大图预览事件
+  var wrapper = $(item);
+  wrapper.on('click', function(e) {
+    //  默认事件
+    e.preventDefault();
+
+    //  清楚之前的图片浏览器
+
+    $('.photo-browser').remove();
+
+
+    //  拿到图片链接
+    var urls = [];
+    var images = $(this).children('canvas');
+    images.forEach(function(item, index) {
+      urls.push(item.toDataURL());
+    })
+    console.log(urls);
+    var myPhotoBrowserStandalone = $.photoBrowser({
+      photos: urls
+    })
+
+    // 打开图片浏览器
+    myPhotoBrowserStandalone.open();
+
+    // myPhotoBrowserStandalone.attachEvents(true);
+    // if (myPhotoBrowserStandalone.params.type === 'standalone') {
+    //   pb.container.removeClass('photo-browser-in').addClass('photo-browser-out').transitionEnd(function () {
+    //   pb.container.removeClass('photo-browser-in').addClass('photo-browser-out').animationEnd(function () { pb.container.remove(); });
+    // }
+  })
+})
+
   }
 
 
