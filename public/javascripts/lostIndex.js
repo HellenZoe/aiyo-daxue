b57fe461ec0f6fb2285633bef158914ace244dae
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-20 12:39:55 +0800

    debug

--- public/javascripts/lostIndex.js
+++ public/javascripts/lostIndex.js
@@ -1,5 +1,29 @@
+//  点击到详情页
 $('.facebook-card').on('click', function(e) {
   var lId = $(this).attr("data-lId");
   var host = location.host;
   location.href = "http://" + host + "/lost/detail/" + lId;
 })
+
+//  切换失物和招领
+$('.gotoLost').on('click', function(e) {
+
+  e.preventDefault();
+
+  $('#lost').toggle(true);  //显示
+  $('#found').toggle(false);   //隐藏
+  $('.lost').toggleClass('active');  //添加active类
+  $('.found').toggleClass('active'); // 删除active类
+
+});
+
+$('.gotoFound').on('click', function(e) {
+
+  e.preventDefault();
+
+  $('#lost').toggle(false);  //显示
+  $('#found').toggle(true);   //隐藏
+  $('.lost').toggleClass('active');  //添加active类
+  $('.found').toggleClass('active'); // 删除active类
+
+});
