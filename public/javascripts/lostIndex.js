0a80183996083e633281f6ad744140ae9f753fa5
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-20 12:58:31 +0800

    debug

--- public/javascripts/lostIndex.js
+++ public/javascripts/lostIndex.js
@@ -10,22 +10,36 @@ $('.gotoLost').on('click', function(e) {
 
   e.preventDefault();
 
+  // 切换内容
   $('#lost').toggle(true);  //显示
   $('#found').toggle(false);   //隐藏
+
+  //  切换当前标签
   $('.lost span').removeClass('active');  //删除active类
   $('.found span ').removeClass('active'); // 删除active类
   $('.lost span').addClass('active');  //添加active类
 
+  // 激活第一个标签
+  $('#lost .button').removeClass('active');
+  $('#lost .button:first-child').addClass('active');
 });
 
 $('.gotoFound').on('click', function(e) {
 
   e.preventDefault();
 
+  // 切换内容
   $('#lost').toggle(false);  //显示
   $('#found').toggle(true);   //隐藏
+
+  //  切换当前标签
   $('.lost span').removeClass('active');  //删除active类
   $('.found span').removeClass('active'); // 删除active类
   $('.found span').addClass('active');  //添加active类
+
+
+  // 激活第一个标签
+  $('#found .button').removeClass('active');
+  $('#found .button:first-child').addClass('active');
 
 });
