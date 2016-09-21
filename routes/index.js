c3d78c349ce7ff431ed95ba2232109a7a8b15a1b
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-21 20:02:30 +0800

    debug

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ module.exports = function(app) {
   })
 
   app.get('/test', function(req, res) {
-    var html = fs.readFileSync('/root/app/aiyo-daxue/test/第一篇文章.html', 'utf-8');
+    var html = fs.readFileSync('/root/app/aiyo-daxue/test/test.html', 'utf-8');
     res.send(html);
     // fs.readFile('/root/files/test.html', function(err, data) {
     //   if (err) {
