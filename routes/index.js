f3b52f95f405d5272fafcfae142d6cf2c8c1c362
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-22 10:27:44 +0800

    debug

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var fs = require('fs');
 var multer= require('multer');
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/upload/tmp')
+    cb(null, '../upload/tmp')
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())
