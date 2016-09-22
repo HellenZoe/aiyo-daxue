a419b4f4e5b3b2f978ee7090806d6ffd889fc4f6
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-22 18:45:07 +0800

    debug

--- admin/dist/js/schoolPrattle.js
+++ admin/dist/js/schoolPrattle.js
@@ -3,34 +3,7 @@
  * Copyright 2013-2016 Start Bootstrap
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
  */
-//  展示已经发不过的文章
-$(document).ready(function() {
-  var url = "http://" + location.host + "/admin/schoolPrattle";
-  $('#dataTables-prattle').DataTable({
-      "processing": true,
-      "serverSide": true,
-      "ajax": {
-        url: url,
-        type: "GET",
-        dataType: "json",
-  			contentType: "application/json",
-        success: function(data) {
-        }
-      },
-      "columns": [
-        {"data": "title"},
-        {"data": "author"},
-        {"data": "time"},
-        {"data": "view"},
-        {
-          "data": "_id",
-          "render": function(data, type, row) {
-            return "<button class='btn btn-primary'>删除</button>"
-          }
-        }
-      ]
-  });
-});
+
 
 $('#prattle-submit').on('click',function(e) {
   e.preventDefault();
