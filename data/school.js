c46b1647f0fca23f173fa89509799db0bcd1e53e
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-28 16:16:28 +0800

    debug

--- data/school.js
+++ data/school.js
@@ -1,7 +1,7 @@
 var schoolList = [
   {id: 0, name: "武汉东湖学院"},
-  {id: 1, name: "武汉汉口学院"}
-  // {id: 2, name: "武汉理工大学"}
+  {id: 1, name: "武汉汉口学院"},
+  {id: 2, name: "其他学校"}
 ]
 
 module.exports = schoolList
