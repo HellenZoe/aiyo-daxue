ea802b1a9d2f78ef036a032bbb724424e92b80a1
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-29 14:34:55 +0800

    debug

--- src/js/treeholeSelf.js
+++ src/js/treeholeSelf.js
@@ -23,7 +23,7 @@ allCard.forEach(function(item, index) {
 
 //  删除树洞
 $('.delete').on('click', function(e) {
-  //  阻止a标签的默认事件
+  //  停止冒泡
   e.stopPropagation();
   var that = this;
   var id = $(this).attr('data-tid');
