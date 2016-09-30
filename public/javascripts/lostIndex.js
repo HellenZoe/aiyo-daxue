beccc4b36792b79cf504035f34b02e72c2ce400a
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-30 11:02:30 +0800

    debug

--- public/javascripts/lostIndex.js
+++ public/javascripts/lostIndex.js
@@ -4,40 +4,40 @@ if (window.utils.getFromLocal('lost') == "found") {
   location.reload();
 }
 //  切换失物和招领
-$('.gotoLost').on('click', function(e) {
-
-  e.preventDefault();
-
-  // 切换内容
-  $('#lost').toggle(true);  //显示
-  $('#found').toggle(false);   //隐藏
-
-  //  切换当前标签
-  $('.lost span').removeClass('active');  //删除active类
-  $('.found span ').removeClass('active'); // 删除active类
-  $('.lost span').addClass('active');  //添加active类
-
-  // 激活第一个标签
-  $('#lost .button').removeClass('active');
-  $('#lost .button:first-child').addClass('active').trigger('click');
-});
-
-$('.gotoFound').on('click', function(e) {
-
-  e.preventDefault();
-
-  // 切换内容
-  $('#lost').toggle(false);  //显示
-  $('#found').toggle(true);   //隐藏
-
-  //  切换当前标签
-  $('.lost span').removeClass('active');  //删除active类
-  $('.found span').removeClass('active'); // 删除active类
-  $('.found span').addClass('active');  //添加active类
-
-
-  // 激活第一个标签
-  $('#found .button').removeClass('active');
-  $('#found .button:first-child').addClass('active').trigger('click');
-
-});
+// $('.gotoLost').on('click', function(e) {
+//
+//   e.preventDefault();
+//
+//   // 切换内容
+//   $('#lost').toggle(true);  //显示
+//   $('#found').toggle(false);   //隐藏
+//
+//   //  切换当前标签
+//   $('.lost span').removeClass('active');  //删除active类
+//   $('.found span ').removeClass('active'); // 删除active类
+//   $('.lost span').addClass('active');  //添加active类
+//
+//   // 激活第一个标签
+//   $('#lost .button').removeClass('active');
+//   $('#lost .button:first-child').addClass('active').trigger('click');
+// });
+//
+// $('.gotoFound').on('click', function(e) {
+//
+//   e.preventDefault();
+//
+//   // 切换内容
+//   $('#lost').toggle(false);  //显示
+//   $('#found').toggle(true);   //隐藏
+//
+//   //  切换当前标签
+//   $('.lost span').removeClass('active');  //删除active类
+//   $('.found span').removeClass('active'); // 删除active类
+//   $('.found span').addClass('active');  //添加active类
+//
+//
+//   // 激活第一个标签
+//   $('#found .button').removeClass('active');
+//   $('#found .button:first-child').addClass('active').trigger('click');
+//
+// });
