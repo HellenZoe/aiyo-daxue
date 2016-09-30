f104ca82aac03f44507fa056f2cb259c3ef74b1c
liadbiz <zhuhaihao_hust@firefox.com>
2016-09-30 17:30:43 +0800

    debug

--- public/javascripts/treeholePost.js
+++ public/javascripts/treeholePost.js
@@ -17,7 +17,7 @@ $(function() {
   // hack 默认的上传文件的样式  然后用另外一个图标的点击来触发
   var newFileIcon = $('.iconfont-treeholePost');
   newFileIcon.on('click', function() {
-    if (formInfo.pics.length >= 4) {
+    if (allPic.pics.length >= 4) {
       $.hidePreloader();
       $.toast("最多添加四张照片");
       return false;
