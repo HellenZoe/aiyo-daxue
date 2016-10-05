4e8e59551713448c3beb85624bc2947d66d51b5c
liadbiz <zhuhaihao_hust@firefox.com>
2016-10-05 09:17:58 +0800

    debug

--- public/javascripts/playPost.js
+++ public/javascripts/playPost.js
@@ -386,8 +386,9 @@ $(function() {
   // })
   //
   $(document).on('ajaxError', function(e, xhr,options, error) {
-    alert(xhr.status);
+    alert(parseInt(xhr.status) == 0);
     if (parseInt(xhr.status) == 0) {
+      $.hidePreloader();
       $.alert("现在返回主页", "发布成功", function() {
         var host = location.host;
         // console.log("http://" + host + "/play");
