fd880c3cb7d98f72e2c1aedeeedd87a7a11a76fd
Yut <yut.hello.world@gmail.com>
2016-10-19 04:46:46 +0800

    查看userInfo

--- routes/index.js
+++ routes/index.js
@@ -79,7 +79,7 @@ module.exports = function(app) {
                 if (err) {
                     console.log(err);
                 }else {
-                    console.log("*************logging from /self************userinfo", doc[0]);
+                    console.log("*************logging from /self************userInfo :", JSON.stringify(doc[0]));
                     res.render('self', {
                         title: "个人信息",
                         userInfo: doc[0]
