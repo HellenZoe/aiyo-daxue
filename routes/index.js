a087a86bca8fb9ddfb18be30b972fef86df5905f
Yut <yut.hello.world@gmail.com>
2016-10-18 18:32:09 +0800

    修改登陆后跳转到个人中心

--- routes/index.js
+++ routes/index.js
@@ -66,8 +66,8 @@ module.exports = function(app) {
 
   //  登陆成功页面
   app.get('/welcome', function(req, res) {
-    res.render('index', {
-      title: "首页",
+    res.render('self', {
+      title: "个人信息",
     })
   })
 
